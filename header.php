253563070e9a2f3ac7f8a819a5d286b00204fbc7
sinhvien <sinhvien>
2021-04-05 13:41:15 +0700

    them header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="vovanthong" />
+    <link href="/LAB3/site.css" rel="stylesheet" />
+
+    <title>website ban hang</title>
+    <style>
+
+</style>
+</head>
+<body>
+    <div id=wrapper>
+        <h2>project training - xay dung website ban hang </h2>
+       
+    </div>
+</body>
+</html>
